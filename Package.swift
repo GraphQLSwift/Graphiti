1db83d76cabe0d1af2034af6f6014818d3d920fb
Mike Pollard <mike.pollard@clubspark.com>
2019-01-24 09:22:52 +0000

    ref master branch of GraphQL

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
 
     dependencies: [
-    .package(url: "https://github.com/SportlabsTechnology/GraphQL.git", .revision("0fb10c111e7593884084ed9d7e3c7ea1012260d9")),
+    .package(url: "https://github.com/SportlabsTechnology/GraphQL.git", .branch("master")),
     ],
 
     targets: [
