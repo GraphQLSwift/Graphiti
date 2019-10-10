bed966b406f2b7b4fedbafd39e67a8ad97a67d78
Paulo Faria <paulo.faria.rl@gmail.com>
2019-10-10 15:18:07 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .library(name: "Graphiti", targets: ["Graphiti"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/GraphQLSwift/GraphQL.git", .upToNextMinor(from: "0.11.0")),
+        .package(url: "https://github.com/GraphQLSwift/GraphQL.git", .upToNextMinor(from: "0.12.0")),
         .package(url: "https://github.com/wickwirew/Runtime.git", .upToNextMinor(from: "2.1.0"))
     ],
     targets: [
