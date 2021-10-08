414ebc2b4526caf8a34750985aabc680dc7d31e9
Jay Herron <NeedleInAJayStack@protonmail.com>
2021-10-08 13:37:31 -0600

    Makes encoder and decoder public

--- Sources/Graphiti/Coder/Coders.swift
+++ Sources/Graphiti/Coder/Coders.swift
@@ -4,8 +4,8 @@ import Foundation
 /// Struct containing a MapEncoder and MapDecoder. These decoders are passed through to the Schema objects and used in
 /// all encoding and decoding from maps.
 public struct Coders {
-    let decoder = MapDecoder()
-    let encoder = MapEncoder()
+    public let decoder = MapDecoder()
+    public let encoder = MapEncoder()
     
     public init() { }
 }
