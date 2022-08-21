3fcc525897612fd2bd42f7d2cb685f5ecb7c9a4c
Jay Herron <NeedleInAJayStack@protonmail.com>
2022-08-21 00:31:49 -0600

    chore: Removes unnecessary linux test file

--- Tests/LinuxMain.swift
+++
@@ -1,8 +0,0 @@
-@testable import GraphitiTests
-import XCTest
-
-XCTMain([
-    testCase(HelloWorldTests.allTests),
-    testCase(StarWarsQueryTests.allTests),
-    testCase(StarWarsIntrospectionTests.allTests),
-])
