232b16cf4b11f0a91353e52845ec96c275e82bed
Sami Suteria <sami@no.dev>
2023-01-26 19:01:41 -0800

    add array based init for unions

---
+++ Tests/GraphitiTests/UnionTests.swift
@@ -0,0 +1,59 @@
+import Foundation
+@testable import Graphiti
+import GraphQL
+import NIO
+import XCTest
+
+class UnionTests: XCTestCase {
+    func testUnionInit() throws {
+        _ = try Schema<StarWarsResolver, StarWarsContext> {
+            Type(Planet.self) {
+                Field("id", at: \.id)
+            }
+
+            Type(Human.self) {
+                Field("id", at: \.id)
+            }
+
+            Type(Droid.self) {
+                Field("id", at: \.id)
+            }
+
+            Union(SearchResult.self, members: Planet.self, Human.self, Droid.self)
+
+            Query {
+                Field("search", at: StarWarsResolver.search, as: [SearchResult].self) {
+                    Argument("query", at: \.query)
+                }
+            }
+        }
+
+
+        _ = try Schema<StarWarsResolver, StarWarsContext> {
+            Type(Planet.self) {
+                Field("id", at: \.id)
+            }
+
+            Type(Human.self) {
+                Field("id", at: \.id)
+            }
+
+            Type(Droid.self) {
+                Field("id", at: \.id)
+            }
+
+            Union(SearchResult.self, members: [
+                Planet.self,
+                Human.self,
+                Droid.self,
+            ])
+
+            Query {
+                Field("search", at: StarWarsResolver.search, as: [SearchResult].self) {
+                    Argument("query", at: \.query)
+                }
+            }
+        }
+
+    }
+}
