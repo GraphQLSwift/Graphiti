2b311667e46dff642e1a8d97e6060a3440094ce7
Sami Suteria <sami@no.dev>
2023-02-07 21:08:41 -0800

    make resolver work with federation

--- Tests/GraphitiTests/ProductAPI/ProductResolver.swift
+++ Tests/GraphitiTests/ProductAPI/ProductResolver.swift
@@ -1,6 +1,10 @@
 import Foundation
+import Graphiti
+import NIO
 
 struct ProductResolver {
+    var sdl: String
+
     struct ProductArguments: Codable {
         let id: String
     }
@@ -16,5 +20,39 @@ struct ProductResolver {
 
     func deprecatedProduct(context: ProductContext, arguments: DeprecatedProductArguments) -> DeprecatedProduct? {
         context.getDeprecatedProduct(sku: arguments.sku, package: arguments.package)
+    }
+}
+
+extension ProductResolver: FederationResolver {
+    static let encoder = JSONEncoder()
+    static let decoder = JSONDecoder()
+    static let entityKeys: [(entity: FederationEntity.Type, keys: [FederationEntityKey.Type])] = [
+        (Product.self, [Product.EntityKey1.self, Product.EntityKey2.self, Product.EntityKey3.self]),
+        (DeprecatedProduct.self, [DeprecatedProduct.EntityKey.self]),
+        (ProductResearch.self, [ProductResearch.EntityKey.self]),
+        (ProductUser.self, [ProductUser.EntityKey.self]),
+    ]
+
+    func entity(context: ProductContext, key: FederationEntityKey, group: EventLoopGroup) -> EventLoopFuture<FederationEntity?> {
+        var entity: FederationEntity? = nil
+
+        switch key {
+        case let key as Product.EntityKey1:
+            entity = context.getProduct(id: key.id)
+        case let key as Product.EntityKey2:
+            entity = context.getProduct(sku: key.sku, package: key.package)
+        case let key as Product.EntityKey3:
+            entity = context.getProduct(sku: key.sku, variationID: key.variation.id)
+        case let key as DeprecatedProduct.EntityKey:
+            entity = context.getDeprecatedProduct(sku: key.sku, package: key.package)
+        case let key as ProductResearch.EntityKey:
+            entity = context.getProductResearch(studyCaseNumber: key.study.caseNumber)
+        case let key as ProductUser.EntityKey:
+            entity = context.getUser(email: key.email)
+        default:
+            break
+        }
+
+        return group.next().makeSucceededFuture(entity)
     }
 }
