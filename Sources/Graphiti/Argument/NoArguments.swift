681001ccf93092170e51b4508056fc9038cd6e9f
Chris Shadek <cshadek@vegaconnected.com>
2023-10-13 15:06:17 -0500

    Made inits public for NoArguments and PageInfo

--- Sources/Graphiti/Argument/NoArguments.swift
+++ Sources/Graphiti/Argument/NoArguments.swift
@@ -1,3 +1,3 @@
 public struct NoArguments: Decodable {
-    init() {}
+    public init() {}
 }
--- Sources/Graphiti/Connection/PageInfo.swift
+++ Sources/Graphiti/Connection/PageInfo.swift
@@ -3,4 +3,11 @@ public struct PageInfo: Codable {
     public let hasNextPage: Bool
     public let startCursor: String?
     public let endCursor: String?
+    
+    public init(hasPreviousPage: Bool, hasNextPage: Bool, startCursor: String? = nil, endCursor: String? = nil) {
+        self.hasPreviousPage = hasPreviousPage
+        self.hasNextPage = hasNextPage
+        self.startCursor = startCursor
+        self.endCursor = endCursor
+    }
 }
